cd356c5bb68169671739bc4bb3e923a33326ea02
CryptoJoe33 <117700080+CryptoJoe33@users.noreply.github.com>
2023-09-14 16:46:33 -0600

    Finalized week 2 Assignment
    Finished assignment and added shopping list page.

--- app/StudentInfo.jsx
+++ app/StudentInfo.jsx
@@ -1,9 +1,11 @@
+import Link from 'next/link';
+
 export default function MyInfo(){
     return (
         <div>
             <h1>Joey Adam</h1>
             <h1>CPRG 306</h1>
-            <link href="https://github.com/CryptoJoe33"/>
+            <Link href ="https://github.com/CryptoJoe33">Github</Link>
         </div>
-    )
+    );
 }
---
+++ app/week2/page.js
@@ -0,0 +1,11 @@
+import StudentInfo from '../StudentInfo';
+import Link from 'next/link';
+
+export default function ShoppingList(){
+    return (
+        <div>
+            <StudentInfo />
+            <h1>My Shopping List</h1>
+        </div>
+    )
+}
