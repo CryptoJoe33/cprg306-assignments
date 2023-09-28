d463db18ca648c8530970e5278e9727bb791b280
CryptoJoe33 <117700080+CryptoJoe33@users.noreply.github.com>
2023-09-27 20:51:12 -0600

    Updated page

--- app/week3/item.js
+++ app/week3/item.js
@@ -1,7 +1,6 @@
 export default function Item({name, quantity, category}){
     return (
         <ul className="m-2 p-2 rounded border border-white bg-pink-400 max-w-xs text-justify">
-            <li></li>
             <li>Purchase: {name}</li>
             <li>Amount: {quantity} from category: {category}</li>
         </ul>
