dc2a87cf0412037cb9d7d923b0efd0b4307e0e30
CryptoJoe33 <117700080+CryptoJoe33@users.noreply.github.com>
2023-09-28 23:08:52 -0600

    Added Styling
    May or may not keep as it looks cool but stupid at the same time

--- app/week4/new-item.js
+++ app/week4/new-item.js
@@ -2,7 +2,7 @@
 
 import { useState } from "react"
 
-export default function NewItem(){
+export default function NewItem() {
     const [name, setName] = useState("");
     const [quantity, setQuantity] = useState(0);
     const [category, setCategory] = useState("Produce");
@@ -39,58 +39,57 @@ export default function NewItem(){
 
 
     return (
-        <main>
-            <div>
-                <div className="text-black m-4">
-                    <form onSubmit={handleSubmit}/>
-                </div>
-        
-                <div className="text-black m-4">
-                    <input className=" focus:outline-none focus:ring-2 focus:ring-green-500 focus:border-transparent"
-                    required
-                    type="text" 
-                    placeholder="Enter item name"
-                    value={name} 
-                    onChange = {handleNameChange}
-                    />      
-                </div>
-
-                <div className=" text-black m-4">
-                    <input className="focus:outline-none focus:ring-2 focus:ring-green-500 focus:border-transparent"
-                    required
-                    type="number" 
-                    placeholder="Enter quantity"
-                    value={quantity} 
-                    onChange = {handleQuantityChange}
-                    />
-                </div>
-                
-                <div className=" text-black m-4">
-                    <select className="focus:outline-none focus:ring-2 focus:ring-green-500 focus:border-transparent"
-                    required
-                    type="text"
-                    placeholder="Enter category"
-                    value={category}
-                    onChange={handleCategoryChange}
-                    >
-                        <option value="Produce">Produce</option>
-                        <option value="Dairy">Dairy</option>
-                        <option value="Bakery">Bakery</option>
-                        <option value="Meat">Meat</option>
-                        <option value="Canned Goods">Canned Goods</option>
-                        <option value="Dry Goods">Dry Goods</option>
-                        <option value="Beverages">Beverages</option>
-                        <option value="Snacks">Snacks</option>
-                        <option value="Household">Household</option>
-                        <option value="Other">Other</option>
-                    </select>
-                </div>
-               
-               <div className="text-black">
-                    <button onClick={handleSubmit}>Submit</button>
-               </div>
-            
+    <main>
+        <div>
+            <div className="text-black m-4">
+                <form onSubmit={handleSubmit}/>
             </div>
-        </main>
+    
+            <div className="text-black m-4">
+                <input className=" focus:outline-none focus:ring-2 focus:ring-green-500 focus:border-transparent"
+                required
+                type="text" 
+                placeholder="Enter item name"
+                value={name} 
+                onChange = {handleNameChange}
+                />      
+            </div>
+
+            <div className=" text-black m-4">
+                <input className="focus:outline-none focus:ring-2 focus:ring-green-500 focus:border-transparent"
+                required
+                type="number" 
+                placeholder="Enter quantity"
+                value={quantity} 
+                onChange = {handleQuantityChange}
+                />
+            </div>
+            
+            <div className=" text-black m-4">
+                <select className="focus:outline-none focus:ring-2 focus:ring-green-500 focus:border-transparent"
+                required
+                type="text"
+                placeholder="Enter category"
+                value={category}
+                onChange={handleCategoryChange}
+                >
+                    <option value="Produce">Produce</option>
+                    <option value="Dairy">Dairy</option>
+                    <option value="Bakery">Bakery</option>
+                    <option value="Meat">Meat</option>
+                    <option value="Canned Goods">Canned Goods</option>
+                    <option value="Dry Goods">Dry Goods</option>
+                    <option value="Beverages">Beverages</option>
+                    <option value="Snacks">Snacks</option>
+                    <option value="Household">Household</option>
+                    <option value="Other">Other</option>
+                </select>
+            </div>
+            
+            <div >
+                <button onClick={handleSubmit} className="text-black m-4 border-black bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500 rounded">Submit</button>
+            </div>
+        </div>
+    </main>
     )
 };
--- app/week4/page.js
+++ app/week4/page.js
@@ -3,7 +3,7 @@ import NewItem from "./new-item.js";
 export default function Page(){
     return (
         <main>
-            <h1 className="text-pink-300 m-4">Add an Item!!</h1>
+            <h1 className="text-pink-300 m-4 ">Add an Item!!</h1>
             <NewItem/>
         </main>
     );
