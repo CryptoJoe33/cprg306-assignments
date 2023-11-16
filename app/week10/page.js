d74f16ce81af3f4a2a85953f8c657d4fbc235d0b
CryptoJoe33 <117700080+CryptoJoe33@users.noreply.github.com>
2023-11-16 16:34:58 -0700

    Added files
    Copied week 8 into the week 10 folder

---
+++ app/week10/page.js
@@ -0,0 +1,30 @@
+"use client";
+
+import { useUserAuth } from "./_utils/auth-context";
+
+export default function Page() {
+
+    const {user, gitHubSignIn, firebaseSignOut} = useUserAuth();
+
+    async function handleSignIn() {
+        await gitHubSignIn();
+    }
+    
+    async function handleSignOut() {
+        await firebaseSignOut();
+    }
+
+    return (
+        <div>
+            <div>
+                <h1>Welcome to Week 8!</h1>
+                {!user && <h1>Sign in to View Week 8</h1>}
+                {!user && <button onClick = {handleSignIn}>Sign In</button>}
+            </div>
+            <div>
+                {user && <h1>Sign Out if You're a Loser!</h1>}
+                {user && <button onClick = {handleSignOut}>Sign Out</button>}
+            </div>
+        </div>
+    )
+}
